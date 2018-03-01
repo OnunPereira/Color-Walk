cd022560a5ce130de8470a685082c387ac468626
OnunPereira <nuno.pereira.as@gmail.com>
2018-03-01 12:07:01 +0000

    first commit yey

---
+++ index.js
@@ -0,0 +1,23 @@
+const generateColor = () => {
+
+    const colorArray = ["cyan", "magenta", "yellow", "green", "purple", "red"];
+    let randomIndex = Math.floor(6 * Math.random());
+    let color = colorArray[randomIndex];
+    return color;
+
+}
+
+const generateMap = () => {
+
+    const mainDiv = document.getElementById('wrapper'); 
+
+    for (let i = 0; i < 600; i++) {
+        mainDiv.innerHTML += '<div class="block" id="block' + i + '"></div>';
+    }
+};
+
+const onLoad = () => {
+    generateMap();
+}
+
+document.body.onload = onLoad;
